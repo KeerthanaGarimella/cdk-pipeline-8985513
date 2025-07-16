ff5252e25998e85fa11b1d9f976ab76cbf4acba3
KeerthanaGarimella <kgarimella5513@conestogac.on.ca>
2025-07-15 22:13:18 -0400

    Initial commit

---
+++ test/cdk-pipeline-8985513.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipeline8985513 from '../lib/cdk-pipeline-8985513-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-8985513-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipeline8985513.CdkPipeline8985513Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
